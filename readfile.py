d3fd1b915741f23ab09219886f7d87af9f5f05cc
knguyen-academy <knguyen.academy@gmail.com>
2017-04-13 16:39:21 -0700

    Modified to work with gyro files- still not work

--- readfile.py
+++ readfile.py
@@ -1,19 +1,27 @@
 print("Program started.")
-file1 = open("test3.txt", "r")
+file1 = open("test1.txt", "r")
 out_files=[]
-for i in range(0,6):
+for i in range(0,7):
     f = open('out'+ str(i) + '.txt', "a")
     out_files.append(f)
 
 for line in file1:
     #line = file1.readline()
-    print(line)
+    #print(line)
     x=line.split(', ')
-    for i in range(0,6):
+    print len(x)
+    for i in range(0,7):
         #print('File' +str(i))
-        if 'File' + str(i) in x:
+        if len(x) == 4:
+            out_files[6].write(x[1] + ', ' + x[2] + ', ' + x[3] )
+        else:
+            if 'File' + str(i) in x :
             #print('Found File' + str(i))
-            out_files[i].write(x[1] + ', ' + x[2])
+                out_files[i].write(x[1] + ', ' + x[2])
+        #else:
+                #print(line)
+            #out_files[6].write(x[1] + ', ' + x[2] + ', ' + x[3] )
+        
             
     
     
